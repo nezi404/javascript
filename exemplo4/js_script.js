90fd90b805f8a69a2b99096028b9529f35840e8d
nezi404 <105327627+nezi404@users.noreply.github.com>
2023-07-19 11:50:55 -0400

    Add files via upload

---
+++ exemplo4/js_script.js
@@ -0,0 +1,23 @@
+let resp = window.document.getElementById("saida")
+
+function acao1() {
+    resp.innerHTML += "<p>Voce clicou no pimeiro botao</p>"
+    console.log("1")
+}
+function acao2() {
+    resp.innerHTML += "<p>Voce clicou no segundo botao</p>"
+    console.log("2")
+
+}
+
+function acao3() {
+    resp.innerHTML += "<p>Voce clicou no terceiro botao</p>"
+    console.log("3")
+
+}
+
+function acao3() {
+    resp.innerHTML += "<p>Voce clicou no quarto botao</p>"
+    console.log("4")
+
+}
